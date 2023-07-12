1f06e853eb7459decd006b8f7ef1c1d9c30fa463
Ioana Caraman <ioanacaraman3@gmail.com>
2023-07-12 19:07:22 +0300

    Hangman - JS - commit after codereview - code refactor

--- src/index.js
+++ src/index.js
@@ -16,35 +16,39 @@ function generateUnderscores() {
 function playHangman() {
     let inputValue = document.getElementById('chooseLetter').value.toLowerCase();
     let correctLetter = false;
-        if (inputValue.length === 1 && inputValue.match(/[a-z]/i)) {
-            for (let i = 0; i < word.length; ++i) {
-                if (word[i] === inputValue) {
-                    let letterElement = document.getElementById('underscore_'+ i);
-                    letterElement.innerText=inputValue.toUpperCase();
-                    correctLetter = true;
-                    ++correctLettersCounter;
-                }
+
+    if (inputValue.length === 1 && inputValue.match(/[a-z]/i)) {
+        for (let i = 0; i < word.length; ++i) {
+            if (word[i] === inputValue) {
+               let letterElement = document.getElementById('underscore_'+ i);
+               letterElement.innerText=inputValue.toUpperCase();
+               correctLetter = true;
+               ++correctLettersCounter;
             }
-                    if (correctLettersCounter === word.length) {
-                        let gameWinning = document.getElementById('gameOverText');
-                        gameWinning.innerText = "Win!";
-                        document.getElementById("playGame").disabled = true;
-                    }
         }
-                        if (!correctLetter) {
-                            lives--;
-                            updateLivesText();
-                        }
+
+        if (correctLettersCounter === word.length) {
+            let gameWinning = document.getElementById('gameOverText');
+            gameWinning.innerText = "Win!";
+            document.getElementById("playGame").disabled = true;
+        }
+    }
+
+    if (!correctLetter) {
+       lives--;
+       updateLivesText();
+    }
 }
 
 function updateLivesText() {
     let livesWarning = document.getElementById('livesText');
     livesWarning.innerText = "You have " + lives + " lives left";
-        if (lives === 0) {
-            let gameOverWarning = document.getElementById('gameOverText');
-            gameOverWarning.innerText = "Game Over!";
-            document.getElementById("playGame").disabled = true;
-        }
+
+    if (lives === 0) {
+        let gameOverWarning = document.getElementById('gameOverText');
+        gameOverWarning.innerText = "Game Over!";
+        document.getElementById("playGame").disabled = true;
+    }
 }
 
 window.addEventListener("load", (event) => {
